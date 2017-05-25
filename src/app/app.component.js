c1cc21bbfb381008fc1cf50b72aea63106380190
Samir Akre <sakre@ucdavis.edu>
2017-05-25 16:05:02 -0700

    upload genBank file functionality added

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -36,6 +36,12 @@ var AppComponent = (function () {
         this.svgCrowBar.type = "text/javascript";
         this.svgCrowBar.src = "svgCrowbar.js";
         this.svgCrowBar.id = "crowbar";
+        if (window.File && window.FileReader && window.FileList && window.Blob) {
+            console.log('file reader on');
+        }
+        else {
+            alert('The File APIs are not fully supported in this browser.');
+        }
     }
     AppComponent.prototype.addGene = function () {
         if (this.tempGene.length && this.tempGene.direction) {
@@ -84,7 +90,7 @@ var AppComponent = (function () {
             var aY2 = 50 - this.blockHeight / 2;
             var labelX = x1 + bWidth / 2;
             var labelY = aY1 + bH;
-            var rotate = "rotate(60 " + labelX + " " + labelY + ")";
+            var rotate = "rotate(85 " + labelX + " " + labelY + ")";
             var arrow = aX1 + ',' + aY1 + ' ' + aX1 + ',' + aY2 + ' ' + aX2 + ',' + 50;
         }
         if (gene.direction == 'backward') {
@@ -100,7 +106,7 @@ var AppComponent = (function () {
             var aY2 = 50 - this.blockHeight / 2;
             var labelX = x1 + bWidth / 2;
             var labelY = aY1 + bH;
-            var rotate = "rotate(60 " + labelX + " " + labelY + ")";
+            var rotate = "rotate(85 " + labelX + " " + labelY + ")";
             var arrow = aX1 + ',' + aY1 + ' ' + aX1 + ',' + aY2 + ' ' + aX2 + ',' + 50;
         }
         if (gene.direction == 'nonCoding') {
@@ -116,7 +122,7 @@ var AppComponent = (function () {
             var aY2 = 50 - this.blockHeight / 2;
             var labelX = x1 + bWidth / 2;
             var labelY = aY1 + bH;
-            var rotate = "rotate(60 " + labelX + " " + labelY + ")";
+            var rotate = "rotate(85 " + labelX + " " + labelY + ")";
             var arrow = aX1 + ',' + aY1 + ' ' + aX1 + ',' + aY2 + ' ' + aX2 + ',' + 50;
             gene.color = 'transparent';
         }
@@ -144,6 +150,87 @@ var AppComponent = (function () {
     };
     AppComponent.prototype.SVGdownload = function () {
         document.getElementsByTagName("head")[0].appendChild(this.svgCrowBar);
+    };
+    AppComponent.prototype.onChange = function (event) {
+        var _this = this;
+        var files = event.srcElement.files;
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            _this.files = reader.result;
+            var lines = _this.files.split("gene");
+            var genes = [];
+            var lastGene = [];
+            for (var line in lines) {
+                var match = lines[line].match(/[0-9]+\.\.+[0-9]*/g);
+                var notFull = lines[line].match("ORGANISM");
+                if (match && !notFull) {
+                    if (match.length <= 2) {
+                        var pos = match[0].split("..");
+                        pos[0] = Number(pos[0]);
+                        pos[1] = Number(pos[1]);
+                        var len = pos[1] - pos[0];
+                        var name = lines[line].match(/\/product="+.{1,}/g);
+                        if (name) {
+                            name = name[0].slice(10, -1);
+                        }
+                        else {
+                            name = lines[line].match(/\/locus_tag="+.{1,}/g);
+                            if (name) {
+                                name = name[0].slice(12, -1);
+                            }
+                            else {
+                                name = 'No Name';
+                            }
+                        }
+                        var space = 0;
+                        console.log(lastGene);
+                        if (lastGene) {
+                            space = pos[0] - lastGene[3];
+                        }
+                        if (space < 0) {
+                            if (space > -10) {
+                                space = 0;
+                                var gene = [name, len, pos[0], pos[1], space];
+                                genes.push(gene);
+                                lastGene = gene;
+                            }
+                            else {
+                            }
+                        }
+                        else {
+                            var gene = [name, len, pos[0], pos[1], space];
+                            genes.push(gene);
+                            lastGene = gene;
+                        }
+                    }
+                }
+            }
+            var each;
+            for (each in genes) {
+                var tGene;
+                each = genes[each];
+                if (each[4]) {
+                    tGene = {
+                        name: ' ',
+                        length: each[4],
+                        color: 'black',
+                        direction: 'nonCoding',
+                        arrow: false
+                    };
+                    _this.genes.push(tGene);
+                }
+                tGene = {
+                    name: each[0],
+                    length: each[1],
+                    color: 'black',
+                    direction: 'forward',
+                    arrow: false
+                };
+                _this.genes.push(tGene);
+            }
+            _this.refresh();
+        };
+        reader.readAsText(files[0]);
     };
     return AppComponent;
 }());
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent  {
 	tempGene:Gene;
 	svgCrowBar:any;
 	edit:number;
+	files:any;
 	constructor(private sanitizer:DomSanitizer){
 		this.maxLen=4000;
 		this.currentLen=0;
@@ -38,6 +39,11 @@ export class AppComponent  {
 		this.svgCrowBar.type="text/javascript";
 		this.svgCrowBar.src="svgCrowbar.js";
 		this.svgCrowBar.id="crowbar";
+		if ((<any>window).File && (<any>window).FileReader && (<any>window).FileList && (<any>window).Blob) {
+  			console.log('file reader on');
+		} else {
+		  alert('The File APIs are not fully supported in this browser.');
+		}
 
 
 	}
@@ -95,7 +101,7 @@ export class AppComponent  {
 			var aY2=50-this.blockHeight/2;
 			var labelX=x1+bWidth/2;
 			var labelY=aY1+bH;
-			var rotate="rotate(60 "+labelX+" "+labelY+")";
+			var rotate="rotate(85 "+labelX+" "+labelY+")";
 
 			var arrow=aX1+','+aY1+' '+aX1+','+aY2+' '+aX2+','+50 ;
 
@@ -115,7 +121,7 @@ export class AppComponent  {
 			var aY2=50-this.blockHeight/2;
 			var labelX=x1+bWidth/2;
 			var labelY=aY1+bH;
-			var rotate="rotate(60 "+labelX+" "+labelY+")";
+			var rotate="rotate(85 "+labelX+" "+labelY+")";
 
 			var arrow=aX1+','+aY1+' '+aX1+','+aY2+' '+aX2+','+50 ;
 		}
@@ -133,7 +139,7 @@ export class AppComponent  {
 			var aY2=50-this.blockHeight/2;
 			var labelX=x1+bWidth/2;
 			var labelY=aY1+bH;
-			var rotate="rotate(60 "+labelX+" "+labelY+")";
+			var rotate="rotate(85 "+labelX+" "+labelY+")";
 
 			var arrow=aX1+','+aY1+' '+aX1+','+aY2+' '+aX2+','+50 ;
 			gene.color='transparent'
@@ -158,7 +164,7 @@ export class AppComponent  {
 		this.genes.splice(i,1);
 		this.refresh();
 	}
-	editBlock(i)
+	editBlock(i:number)
 	{
 		this.edit=i;
 	}
@@ -166,6 +172,107 @@ export class AppComponent  {
 		document.getElementsByTagName("head")[0].appendChild(this.svgCrowBar);
 
 
+
+	}
+	onChange(event:any){
+	    var files = event.srcElement.files;
+	    let reader=new FileReader();
+		reader.onload = (e) => {
+		    this.files=reader.result;
+		    var lines = this.files.split("gene");
+		    var genes=[];
+		    var lastGene=[];
+		    for( var line in lines)
+		    {
+		    	
+		    	var match = lines[line].match(/[0-9]+\.\.+[0-9]*/g)
+		    	var notFull = lines[line].match("ORGANISM");
+		    	if(match && !notFull)
+		    	{
+			    	if(match.length <= 2)
+			    	{
+			    		var pos=match[0].split("..");
+			    		pos[0]=Number(pos[0]);
+			    		pos[1]=Number(pos[1]);
+			    		var len=pos[1]-pos[0];
+			    		var name = lines[line].match(/\/product="+.{1,}/g);
+			    		if(name)
+			    		{
+			    			name = name[0].slice(10,-1);
+			    		}
+			    		else
+			    		{
+			    			name = lines[line].match(/\/locus_tag="+.{1,}/g);
+			    			if(name)
+			    			{
+			    				name = name[0].slice(12,-1);
+			    			}
+			    			else{
+			    				name='No Name'
+			    			}
+			    			
+			    		}
+			    		var space = 0;
+			    		
+			    		console.log(lastGene);
+			    		if(lastGene)
+			    		{
+							space=pos[0]- lastGene[3];
+			    		}
+			    		if(space<0)
+			    		{
+			    			if(space>-10)
+			    			{
+			    				space=0
+					    		var gene=[name,len,pos[0],pos[1],space]
+					    		genes.push(gene);
+					    		lastGene=gene;			    				
+			    			}
+			    			else{
+
+			    			}
+			    			
+			    		}
+			    		else{
+				    		var gene=[name,len,pos[0],pos[1],space]
+				    		genes.push(gene);
+				    		lastGene=gene;
+			    		}
+
+			    	}
+		    	}
+		    }
+		    var each:any;
+		    for(each in genes)
+		    {
+		    	var tGene:Gene;
+		    	each=genes[each];
+		    	if(each[4])
+		    	{
+			    	tGene={
+			    		name:' ',
+			    		length:each[4],
+			    		color:'black',
+			    		direction:'nonCoding',
+			    		arrow:false
+			    	}
+			    	this.genes.push(tGene);		    		
+		    	}		    	
+		    	
+		    	tGene={
+		    		name:each[0],
+		    		length:each[1],
+		    		color:'black',
+		    		direction:'forward',
+		    		arrow:false
+		    	}
+		    	this.genes.push(tGene);
+
+		    }
+		    this.refresh();
+		}
+
+		reader.readAsText(files[0]);
 
 	}
 }
